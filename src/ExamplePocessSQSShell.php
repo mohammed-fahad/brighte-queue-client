fa59b38b479f1acc55acef2c8c86d6f4e58af2df
Ali Fareh <ali.fareh@brighte.com.au>
2020-01-30 16:11:25 +1100

    Add exampleSQSProcessShell

---
+++ src/ExamplePocessSQSShell.php
@@ -0,0 +1,29 @@
+<?php
+
+
+require_once "vendor/autoload.php";
+
+use App\queue\factories\QueueClientFactory;
+use App\queue\SqsClient;
+use Brighte\Sqs\SqsMessage;
+
+$config = [
+    'key' => 'Key here',
+    'secret' => 'secret here',
+    'region' => 'region',
+    'queue' => 'queue name here',
+    'isFifo' => true,
+    "provider" => "sqs"
+];
+
+$sqsClient = QueueClientFactory::create($config);
+
+$msg = new SqsMessage("this is a test");
+
+//$strategy = new StrategyA(new FailedMessage($msg, 6, 4));
+
+
+$sqsClient->reject($msg);
+
+exit("stop");
+
