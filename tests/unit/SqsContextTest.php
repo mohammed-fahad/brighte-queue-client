716435be380e14e4661ae4d45a7c2bbf07b55c61
Ali Fareh <ali.fareh@brighte.com.au>
2020-02-03 14:11:28 +1100

    Write tests for SqsContext

---
+++ tests/unit/SqsContextTest.php
@@ -0,0 +1,47 @@
+<?php
+
+
+use BrighteCapital\QueueClient\queue\sqs\SqsConsumer;
+use BrighteCapital\QueueClient\queue\sqs\SqsContext;
+use BrighteCapital\QueueClient\queue\sqs\SqsProducer;
+use Enqueue\Sqs\SqsDestination;
+use Enqueue\Sqs\SqsMessage;
+use PHPUnit\Framework\TestCase;
+
+class SqsContextTest extends TestCase
+{
+
+    /**
+     * @var \BrighteCapital\QueueClient\queue\sqs\SqsContext|\PHPUnit\Framework\MockObject\MockObject
+     */
+    private $context;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->context = $this->getMockBuilder(SqsContext::class)
+            ->disableOriginalConstructor()
+            ->setMethodsExcept(['createMessage', 'createProducer', 'createConsumer'])
+            ->getMock();
+
+    }
+
+    public function testCreateMessage()
+    {
+        $msg = $this->context->createMessage("test body");
+        $this->assertInstanceOf(SqsMessage::class, $msg);
+        $this->assertEquals("test body", $msg->getBody());
+    }
+
+    public function createProducer()
+    {
+        $this->assertInstanceOf(SqsProducer::class, $this->context->createProducer());
+    }
+
+    public function testCreateConsumer()
+    {
+        $this->assertInstanceOf(
+            SqsConsumer::class,
+            $this->context->createConsumer(new SqsDestination("some destination")));
+    }
+}
