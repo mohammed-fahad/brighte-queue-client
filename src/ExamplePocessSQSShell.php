6b7bc1b64ca020ee7ec721518edb22baf857e378
fahad <mohammed.fahad@brighte.com.au>
2020-02-10 16:29:06 +1100

    Example Process Message.

--- src/ExamplePocessSQSShell.php
+++ src/ExamplePocessSQSShell.php
@@ -3,7 +3,6 @@
 require_once "vendor/autoload.php";
 
 use BrighteCapital\QueueClient\queue\BrighteQueueClient;
-use BrighteCapital\QueueClient\queue\factories\StorageFactory;
 use BrighteCapital\QueueClient\strategies\StorageRetryStrategy;
 use Enqueue\Sqs\SqsMessage;
 
@@ -24,25 +23,9 @@ $config = [
 ];
 
 $queueClient = new BrighteQueueClient($config);
-
-
-
-/**
- * SqsMessage
- */
 $msg = new SqsMessage("this is the body");
-$entity = new \BrighteCapital\QueueClient\Storage\MessageEntity($msg);
-$entity->setId('1');
-$entity->setMessage('message changed');
-$storage = \BrighteCapital\QueueClient\container\Container::instance()->get('Storage');
-$storage = StorageFactory::create($config['database']);
-var_dump($storage->update($entity));
-
-die('database work done');
-
 $msg->setMessageGroupId(9);
 $msg->setMessageDeduplicationId(9);
-
 $message = $queueClient->receive(20);
 $queueClient->reject($message, new \BrighteCapital\QueueClient\strategies\Retry(15, 0, StorageRetryStrategy::class));
 var_dump($message); die(0);
