2bc7631f8461f001d728c73dd06dc259a6c3994d
Krishna Rao <krishna.rao@brighte.com.au>
2020-03-13 18:12:20 +1100

    BH-5230 Adding more unit tests

--- tests/unit/Queue/QueueClientTest.php
+++ tests/unit/Queue/QueueClientTest.php
@@ -3,11 +3,16 @@
 namespace tests\unit\Queue\Sqs;
 
 use App\Log\Logger;
+use BrighteCapital\QueueClient\Example\JobManager;
 use BrighteCapital\QueueClient\Example\MySql;
+use BrighteCapital\QueueClient\Job\Job;
+use BrighteCapital\QueueClient\Job\JobManagerInterface;
+use BrighteCapital\QueueClient\Queue\BlockerHandlerInterface;
 use BrighteCapital\QueueClient\Queue\Factories\QueueClientFactory;
 use BrighteCapital\QueueClient\Queue\Factories\StrategyFactory;
 use BrighteCapital\QueueClient\Queue\QueueClient;
 use BrighteCapital\QueueClient\Queue\Sqs\SqsClient;
+use BrighteCapital\QueueClient\Strategies\AbstractRetryRetryStrategy;
 use BrighteCapital\QueueClient\Strategies\BlockerStorageRetryStrategy;
 use BrighteCapital\QueueClient\Strategies\Retry;
 use Doctrine\DBAL\DriverManager;
@@ -15,6 +20,7 @@ use Interop\Queue\Message;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\AbstractLogger;
 use Psr\Log\NullLogger;
+use ReflectionClass;
 
 class QueueClientTest extends TestCase
 {
@@ -88,7 +94,7 @@ class QueueClientTest extends TestCase
     public function testSend()
     {
         $this->client = new QueueClient($this->config, null, null, $this->connection, null, $this->factory);
-        $message = $this->client->createMessage('One ring to rule them all', [], []);
+        $message = $this->client->createMessage('One ring to find them', [], []);
 
         $this->sqsClient
             ->expects($this->once())
@@ -104,7 +110,7 @@ class QueueClientTest extends TestCase
         $this->logger->expects($this->once())->method('debug')->with('Queue message Deleted', $this->anything());
 
         $this->client = new QueueClient($this->config, $this->logger, null, $this->connection, null, $this->factory);
-        $message = $this->client->createMessage('One ring to rule them all', [], []);
+        $message = $this->client->createMessage('One ring to bring them all', [], []);
 
         $this->sqsClient
             ->expects($this->once())
@@ -116,25 +122,69 @@ class QueueClientTest extends TestCase
 
     public function testReject()
     {
-
         /** @var \Interop\Queue\Message $message*/
         $retry = new Retry(5, 5, BlockerStorageRetryStrategy::class);
+
+        $strategy = $this->createMock(AbstractRetryRetryStrategy::class);
+        $strategy
+            ->expects($this->once())
+            ->method('handle');
 
         $strategyFactory = $this->createMock(StrategyFactory::class);
         $strategyFactory
             ->expects($this->any())
             ->method('create')
             ->with($retry)
-            ->willReturn('XXXXXXXXXX');
+            ->willReturn($strategy);
 
         $this->client = new QueueClient($this->config, $this->logger, null, $this->connection, $strategyFactory, $this->factory);
+        $message = $this->client->createMessage('One ring to find them', [], []);
 
-
+        $this->client->reject($message, $retry);
 
     }
 
     public function testProcessMessage()
     {
+        $this->client = $this
+            ->getMockBuilder(QueueClient::class)
+            ->setConstructorArgs([$this->config, $this->logger, null, $this->connection, null, $this->factory])
+            ->setMethods(['receive'])
+            ->getMock();
+
+        $message = $this->client->createMessage('And in darkness bind them', [], []);
+
+        $this->client
+            ->expects($this->once())
+            ->method('receive')
+            ->withAnyParameters()
+            ->willReturn($message);
+
+        $retry = new Retry(5, 5, BlockerStorageRetryStrategy::class);
+        $job = new Job($message, $retry);
+        $job->setRetry($retry);
+
+        $this->logger = $this->createMock(NullLogger::class);
+
+        $blockerHandlerMock = $this->createMock(BlockerHandlerInterface::class);
+        $blockerHandlerMock
+            ->expects($this->once())
+            ->method('checkAndHandle')
+            ->with([$job])
+            ->willReturn(true);
+
+        $class = new ReflectionClass($this->client);
+        $property = $class->getProperty('blockerHandler');
+        $property->setAccessible(true);
+        $property->setValue($this->client->blockerHandler, $blockerHandlerMock);
+
+        $jobManager = $this->createMock(JobManagerInterface::class);
+        $jobManager
+            ->method('create')
+            ->with($message)
+            ->willReturn($job);
+
+        $this->logger->expects($this->never())->method('debug')->with('Queue message start processing', $this->anything());
 
     }
 }
